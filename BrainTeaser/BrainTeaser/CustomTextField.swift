88550a4bc6edaba6608fc1a52a7348d8e605fe4c
ngohongthai <ngohongthai.uet@gmail.com>
2016-05-29 16:51:08 +0700

    BrainTeaser
    Finish lecture4

---
+++ BrainTeaser/BrainTeaser/CustomTextField.swift
@@ -0,0 +1,43 @@
+//
+//  CustomTextField.swift
+//  BrainTeaser
+//
+//  Created by Thái Bô Lão on 5/28/16.
+//  Copyright © 2016 Thái Bô Lão. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomTextField: UITextField {
+    
+    @IBInspectable var inset: CGFloat = 0
+    
+    @IBInspectable var cornerRadius: CGFloat = 5.0 {
+        didSet {
+            setupView()
+        }
+    }
+    
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, inset, inset)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return textRectForBounds(bounds)
+    }
+    
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 5.0
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+    func setupView () {
+        self.layer.cornerRadius = cornerRadius
+    }
+
+}
