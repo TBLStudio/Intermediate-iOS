09b89e7e63f545b0b770bd8dc1b97d6ddeb9f789
TBLStudio <iosdev.tbl@gmail.com>
2016-05-30 17:13:33 +0700

    Drawing
    Easel: Finished lecture34

---
+++ Easel/Easel/SettingVC.swift
@@ -0,0 +1,22 @@
+//
+//  SettingVC.swift
+//  Easel
+//
+//  Created by Thái Bô Lão on 5/30/16.
+//  Copyright © 2016 Thái Bô Lão. All rights reserved.
+//
+
+import UIKit
+
+class SettingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = false
+    }
+
+}
---
+++ Easel/Easel/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Easel
+//
+//  Created by Thái Bô Lão on 5/30/16.
+//  Copyright © 2016 Thái Bô Lão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBarHidden = true
+    }
+
+
+
+}
+
