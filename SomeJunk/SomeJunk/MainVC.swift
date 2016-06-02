532470a6a1a9b24ad50d5863ee9234292613b657
ngohongthai <ngohongthai.uet@gmail.com>
2016-06-03 01:01:36 +0700

    CoreData
    SomeJunk: Setup tableview

---
+++ SomeJunk/SomeJunk/MainVC.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  SomeJunk
+//
+//  Created by Ngo Thai on 6/2/16.
+//  Copyright © 2016 TBLStudio. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension MainVC: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("itemCell", forIndexPath: indexPath) as! ItemCell
+        return cell
+    }
+
+}
+
+extension MainVC: UITableViewDelegate {
+
+}
+
---
+++ SomeJunk/SomeJunk/MaterialView.swift
@@ -0,0 +1,36 @@
+//
+//  MaterialView.swift
+//  SomeJunk
+//
+//  Created by Ngo Thai on 6/3/16.
+//  Copyright © 2016 TBLStudio. All rights reserved.
+//
+
+import UIKit
+
+private var materialKey = false
+extension UIView {
+    
+    @IBInspectable var materialDesign: Bool {
+        get {
+            return materialKey
+        }
+        set {
+            materialKey = newValue
+            
+            if materialKey {
+                self.layer.cornerRadius = 3.0
+                self.layer.shadowOpacity = 0.8
+                self.layer.shadowRadius = 3.0
+                self.layer.shadowOffset = CGSizeMake(0.0, 2.0)
+                self.layer.shadowColor = UIColor(red: 157/255, green: 157/255, blue: 157/255, alpha: 1).CGColor
+            } else {
+                self.layer.cornerRadius = 0
+                self.layer.shadowOpacity = 0
+                self.layer.shadowRadius = 0
+                self.layer.shadowColor = nil
+            }
+        }
+    }
+    
+}
