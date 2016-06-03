bdd39f13d5eec9a71a9cfe172e8479d3966c4d55
TBLStudio <iosdev.tbl@gmail.com>
2016-06-03 15:51:41 +0700

    CoreData
    SomeJunk

--- SomeJunk/SomeJunk/ItemDetailsVC.swift
+++ SomeJunk/SomeJunk/ItemDetailsVC.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
 class ItemDetailsVC: UIViewController {
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: CustomTextField!
+    @IBOutlet weak var priceField: CustomTextField!
+    @IBOutlet weak var detailsField: CustomTextField!
+    
+    var stores = [Store]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        storePicker.delegate = self
+        storePicker.dataSource = self
+        
+        //insertDummyDataForStore()
+        getStores()
 
         // Do any additional setup after loading the view.
     }
@@ -21,6 +34,67 @@ class ItemDetailsVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-
+    @IBAction func saveItemPressed(sender: AnyObject) {
+        
+    }
+    
+    func insertDummyDataForStore() {
+        let store1 = NSEntityDescription.insertNewObjectForEntityForName("Store", inManagedObjectContext: ad.managedObjectContext) as! Store
+        store1.name = "Amazon"
+        let store2 = NSEntityDescription.insertNewObjectForEntityForName("Store", inManagedObjectContext: ad.managedObjectContext) as! Store
+        store2.name = "Ebay"
+        let store3 = NSEntityDescription.insertNewObjectForEntityForName("Store", inManagedObjectContext: ad.managedObjectContext) as! Store
+        store3.name = "Alibaba"
+        let store4 = NSEntityDescription.insertNewObjectForEntityForName("Store", inManagedObjectContext: ad.managedObjectContext) as! Store
+        store4.name = "VatGia"
+        let store5 = NSEntityDescription.insertNewObjectForEntityForName("Store", inManagedObjectContext: ad.managedObjectContext) as! Store
+        store5.name = "Walmart"
+        
+        ad.saveContext()
+    }
+    
+    func getStores() {
+        let fetchRequest = NSFetchRequest(entityName: "Store")
+        do {
+            self.stores = try ad.managedObjectContext.executeFetchRequest(fetchRequest) as! [Store]
+            self.storePicker.reloadAllComponents()
+        } catch {
+            //Handle Error
+        }
+    }
 }
+
+extension ItemDetailsVC: UIPickerViewDelegate {
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Row: \(stores[row].name)")
+    }
+    
+}
+
+extension ItemDetailsVC: UIPickerViewDataSource {
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let store = stores[row]
+        return store.name
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
